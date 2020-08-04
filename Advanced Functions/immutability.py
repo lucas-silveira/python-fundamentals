29a09320812980e04bc84c40e802a31788666851
Lucas Silveira <lugano1995@gmail.com>
2020-08-03 21:31:44 -0300

    immutability functions

---
+++ Advanced Functions/immutability.py
@@ -0,0 +1,9 @@
+values = [30, 10, 25, 70, 100, 94]
+
+print(sorted(values))  # [10, 25, 30, 70, 94, 100]
+print(min(values))  # 10
+print(max(values))  # 100
+print(sum(values))  # 329
+print(list(reversed(values)))  # [94, 100, 70, 25, 10, 30]
+
+print(values)  # [30, 10, 25, 70, 100, 94]
